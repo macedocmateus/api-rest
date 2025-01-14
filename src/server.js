ebf8a9252084b3627817c731ef07e4b6f7fd4668
Mateus Macedo <mateusmacedoeng@gmail.com>
2025-01-14 17:48:30 -0300

    add tsx

---
+++ src/server.js
@@ -0,0 +1,5 @@
+function sum(a, b) {
+    return a + b;
+}
+var result = sum(1, 2);
+console.log('resultado da soma:', result);
--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,6 @@
-console.log('hello world!');
+function sum(a: number, b: number): number {
+    return a + b;
+}
+
+const result: number = sum(1, 2);
+console.log('resultado da soma:', result);
