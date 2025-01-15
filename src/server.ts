fe2536846822b892d647b744139cdeebff88dfb0
Mateus Macedo <mateusmacedoeng@gmail.com>
2025-01-15 08:25:22 -0300

    add recovering params

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,13 @@
-import express from 'express';
+import express, { response } from 'express';
 
 const PORT = 3333;
 
 const app = express();
+
+app.get('/products/:id/:user', (request, response) => {
+    const { id, user } = request.params;
+
+    response.send(`Produto ${id} do usuário ${user}`);
+});
 
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
