9ad03ce2c32ba2986f7f201f0503630ee8e6ed92
Mateus Macedo <mateusmacedoeng@gmail.com>
2025-01-15 08:58:57 -0300

    add middleware global

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
 import express, { response } from 'express';
+import { myMiddleware } from './middlewares/my-middleware.js';
 
 const PORT = 3333;
 
 const app = express();
 
 app.use(express.json());
+
+// Uso do middleware de maneira global
+app.use(myMiddleware);
 
 app.get('/products', (request, response) => {
     const { page, limit } = request.query;
@@ -15,7 +19,9 @@ app.get('/products', (request, response) => {
 app.post('/products', (request, response) => {
     const { name, price } = request.body;
 
-    response.send(`O produto ${name} custa ${price} reais`);
+    // response.send(`O produto ${name} custa ${price} reais`);
+
+    response.status(201).json({ name, price });
 });
 
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
