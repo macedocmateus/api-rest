243581e8ca37913c1ad75a203e3ddb8af07f98fc
Mateus Macedo <mateusmacedoeng@gmail.com>
2025-01-15 08:43:20 -0300

    add http post

--- src/server.ts
+++ src/server.ts
@@ -4,10 +4,18 @@ const PORT = 3333;
 
 const app = express();
 
-app.get('/products/:id/:user', (request, response) => {
-    const { id, user } = request.params;
+app.use(express.json());
 
-    response.send(`Produto ${id} do usuário ${user}`);
+app.get('/products', (request, response) => {
+    const { page, limit } = request.query;
+
+    response.send(`Página ${page} de ${limit}`);
+});
+
+app.post('/products', (request, response) => {
+    const { name, price } = request.body;
+
+    response.send(`O produto ${name} custa ${price} reais`);
 });
 
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
