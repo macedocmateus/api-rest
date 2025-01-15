60033008ccfa3e94f945f1889c37e22eae3b3f77
Mateus Macedo <mateusmacedoeng@gmail.com>
2025-01-15 10:36:50 -0300

    add modify tsconfig and fix bug into package json

---
+++ src/routes/products-routes.ts
@@ -0,0 +1,22 @@
+import { Router } from 'express';
+
+import { myMiddleware } from '../middlewares/my-middleware';
+
+const productsRoutes = Router();
+
+productsRoutes.get('/products', (request, response) => {
+    const { page, limit } = request.query;
+
+    response.send(`Página ${page} de ${limit}`);
+});
+
+// Uso do middleware de maneira especifica
+productsRoutes.post('/products', myMiddleware, (request, response) => {
+    const { name, price } = request.body;
+
+    // response.send(`O produto ${name} custa ${price} reais`);
+
+    response.status(201).json({ name, price, user_id: request.user_id });
+});
+
+export { productsRoutes };
--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,9 @@
-import express, { response } from 'express';
-import { myMiddleware } from './middlewares/my-middleware.js';
+import express from 'express';
+import { routes } from './routes';
 
 const PORT = 3333;
 
 const app = express();
-
 app.use(express.json());
-
-// Uso do middleware de maneira global
-// app.use(myMiddleware);
-
-app.get('/products', (request, response) => {
-    const { page, limit } = request.query;
-
-    response.send(`Página ${page} de ${limit}`);
-});
-
-// Uso do middleware de maneira especifica
-app.post('/products', myMiddleware, (request, response) => {
-    const { name, price } = request.body;
-
-    // response.send(`O produto ${name} custa ${price} reais`);
-
-    response.status(201).json({ name, price, user_id: request.user_id });
-});
 
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
