3fe55abdbab29e36c81b9d0045b08bfdd8ae476d
Mateus Macedo <mateusmacedoeng@gmail.com>
2025-01-16 17:34:08 -0300

    add exception handling

--- src/controllers/ProductsController.ts
+++ src/controllers/ProductsController.ts
@@ -18,6 +18,8 @@ class ProductsController {
     create(request: Request, response: Response) {
         const { name, price } = request.body;
 
+        throw new Error('Erro ao criar um produto');
+
         response.status(201).json({ name, price, user_id: request.user_id });
     }
 }
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,10 @@
-import express from 'express';
+import express, {
+    Request,
+    Response,
+    NextFunction,
+    ErrorRequestHandler,
+} from 'express';
+
 import { routes } from './routes';
 
 const PORT = 3333;
@@ -7,5 +13,12 @@ const app = express();
 app.use(express.json());
 
 app.use(routes);
+
+// Deixando o tratamento de erros por último, pois ele irá tratar todas as funções anteriores
+app.use(
+    (error: Error, request: Request, response: Response, _: NextFunction) => {
+        response.status(500).json({ message: error.message });
+    }
+);
 
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
