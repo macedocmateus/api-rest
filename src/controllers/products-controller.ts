bc891e119ab092925314f96858576b237c38f29c
Mateus Macedo <mateusmacedoeng@gmail.com>
2025-01-20 14:56:56 -0300

    add rules of validation with zod

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -19,8 +19,14 @@ class ProductsController {
 
     create(request: Request, response: Response) {
         const bodySchema = z.object({
-            name: z.string({ required_error: 'Name is required!' }),
-            price: z.number({ required_error: 'Price is required!' }),
+            name: z
+                .string({ required_error: 'Name is required!' })
+                .trim()
+                .min(6, { message: 'Name Must be 6 or more characters' }),
+            price: z
+                .number({ required_error: 'Price is required!' })
+                .positive({ message: 'Price must be positive' })
+                .gte(0),
         });
 
         const { name, price } = bodySchema.parse(request.body);
